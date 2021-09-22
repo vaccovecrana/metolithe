09784a60b4219b9aa9ba95feb1812468bc8ec09d
Jesus Zazueta <jjzazuet@vacco.io>
2021-09-22 02:44:15 -0400

    - Dependency updates. - Patch version bump.

--- mt-test/src/test/java/io/vacco/metolithe/test/MtCombinedSpec.java
+++ mt-test/src/test/java/io/vacco/metolithe/test/MtCombinedSpec.java
@@ -1,8 +1,10 @@
 package io.vacco.metolithe.test;
 
+import j8spec.annotation.DefinedOrder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+@DefinedOrder
 @RunWith(Suite.class)
 @Suite.SuiteClasses({MtAnnotationsSpec.class, MtCodeGenSpec.class, MtDaoSpec.class})
 public class MtCombinedSpec {}
