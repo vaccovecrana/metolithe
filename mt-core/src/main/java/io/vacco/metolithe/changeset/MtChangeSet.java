2b9c15d5312974b32f0a018e34009430472d9a89
Jesus Zazueta <jjzazuet@gmail.com>
2025-06-08 19:27:41 -0400

    - WIP.

---
+++ mt-core/src/main/java/io/vacco/metolithe/changeset/MtChangeSet.java
@@ -0,0 +1,10 @@
+package io.vacco.metolithe.changeset;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MtChangeSet {
+
+  public List<MtChange> changes = new ArrayList<>();
+
+}
